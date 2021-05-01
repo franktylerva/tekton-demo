7d6aff90a49a14368fabbf3b864546a94909a5e9
ftyler <ftyler.ctr@kr.af.mil>
2021-05-01 10:30:59 -0400

    Initial code.

---
+++ src/main/java/com/example/tektondemo/TektonDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.tektondemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TektonDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TektonDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/tektondemo/TektonDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tektondemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TektonDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
